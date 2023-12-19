37f57b38801793b5f9588e0030166e0fda264298
hiyongrui <172808H@mymail.nyp.edu.sg>
2023-12-19 13:55:19 +0800

    rendering, layouts
    by default, nextjs routes is statically rendered
    - unless dynamic function/fetch()
    - dynamic function like user's cookies(), current request headers(), URL's useSearchParams()
    - dynamic fetch(), requests that opt out of caching by setting cache option to 'no-store' or revalidate to 0
    - https://nextjs.org/docs/app/building-your-application/caching
    
    layout
    - don't get re-rendered, can fetch data, but cannot pass it down to children through props
    - remember to render children prop, else will only show the layout
    - https://nextjs.org/docs/app/api-reference/file-conventions/layout
    
    templates
    - same as layout, but re-render every time route change
    - useful for enter/exit animation show Suspense fallback everytime, useEffect logic
    - https://nextjs.org/docs/app/api-reference/file-conventions/template

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,10 @@
+const DashboardLayout = ({ children }) => {
+  return (
+    <div>
+      <h1>dashboard layout</h1>
+      <div>{children}</div>
+    </div>
+  )
+}
+
+export default DashboardLayout
